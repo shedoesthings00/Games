11932961253231a4d2dd2f995ca3b54fcbfed407
Marta <73582921+Marta-24@users.noreply.github.com>
2026-01-29 11:57:49 +0100

    Shooting Done

--- nugget-hell/player.gd
+++ nugget-hell/player.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
-@export var speed: float = 6.0
-@export var max_turn_speed: float = 2.0
-@export var dead_zone: float = 30.0
+@export var move_speed: float = 6.0
+@export var bullet_scene: PackedScene
 
 @onready var cam: Camera3D = get_viewport().get_camera_3d()
+@onready var muzzle: Node3D = $Muzzle
 
 
 func _physics_process(delta: float) -> void:
@@ -12,7 +12,7 @@ func _physics_process(delta: float) -> void:
 
 
 func _process(delta: float) -> void:
-	_rotate_with_mouse_gradual(delta)
+	_shoot()
 
 
 func _move_player(delta: float) -> void:
@@ -21,8 +21,8 @@ func _move_player(delta: float) -> void:
 		"move_forward", "move_back"
 	)
 
-	var direction := Vector3.ZERO
-	
+	var direction: Vector3 = Vector3.ZERO
+
 	if input_dir.x != 0.0:
 		direction += transform.basis.x * input_dir.x
 	if input_dir.y != 0.0:
@@ -31,30 +31,42 @@ func _move_player(delta: float) -> void:
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 
-	velocity = direction * speed
+	velocity.x = direction.x * move_speed
+	velocity.z = direction.z * move_speed
 	velocity.y = 0.0
 
 	move_and_slide()
 
 
+func _shoot() -> void:
+	if Input.is_action_just_pressed("shoot") and bullet_scene and muzzle:
+		var dir: Vector3 = _get_shoot_direction_to_mouse()
+		if dir == Vector3.ZERO:
+			return
 
-func _rotate_with_mouse_gradual(delta: float) -> void:
+		var bullet: RigidBody3D = bullet_scene.instantiate() as RigidBody3D
+		get_parent().add_child(bullet)
+
+		bullet.global_transform.origin = muzzle.global_transform.origin
+		(bullet as Node).call("init_direction", dir)
+
+		print("PLAYER: Disparo, dir =", dir)
+
+
+
+func _get_shoot_direction_to_mouse() -> Vector3:
 	if cam == null:
-		return
+		return Vector3.ZERO
 
 	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+	var from: Vector3 = cam.project_ray_origin(mouse_pos)
+	var ray_dir: Vector3 = cam.project_ray_normal(mouse_pos)
 
-	var player_screen_pos: Vector2 = cam.unproject_position(global_position)
+	var plane: Plane = Plane(Vector3.UP, global_position.y)
+	var hit: Variant = plane.intersects_ray(from, ray_dir)
+	if hit == null:
+		return Vector3.ZERO
 
-	var diff_x: float = mouse_pos.x - player_screen_pos.x
-
-	if abs(diff_x) < dead_zone:
-		return
-
-	var viewport_width: float = get_viewport().size.x
-	var max_dist: float = viewport_width * 0.5
-	var strength: float = clamp(diff_x / max_dist, -1.0, 1.0)
-
-	var turn_amount: float = -strength * max_turn_speed * delta
-
-	rotation.y += turn_amount
+	var dir: Vector3 = (hit as Vector3) - muzzle.global_transform.origin
+	dir.y = 0.0
+	return dir.normalized()
